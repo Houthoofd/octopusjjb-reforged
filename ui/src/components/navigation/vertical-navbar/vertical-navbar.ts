8dd88864076f4b89db8b6ed5625eac14b55ddd81
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-16 17:29:19 +0100

    update

--- ui/src/components/navigation/vertical-navbar/vertical-navbar.ts
+++ ui/src/components/navigation/vertical-navbar/vertical-navbar.ts
@@ -9,61 +9,61 @@ import 'unofficial-pf-v5-wc-icons';
       return html`
         <nav id="sidebar">
          <ul id="top-sidebar">
-            <li>
+            <li data-page="accueil">
                <a href="/pages/">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
                   <span>Accueil</span>
                </a>
             </li>
-            <li class="active">
+            <li data-page="cours">
                <a href="/pages/cours">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
                   <span>Cours</span>
                </a>
             </li>
-            <li>
+            <li data-page="dashboard">
                <a href="/pages/dashboard">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z"/></svg>
                   <span>Dashboard</span>
                </a>
             </li>
-            <li>
+            <li data-page="profil">
                <a href="/pages/profil">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
                   <span>Profil</span>
                </a>
             </li>
-            <li>
+            <li data-page="administration">
                <a href="/pages/profil">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm40-120q33 0 56.5-23.5T760-320q0-33-23.5-56.5T680-400q-33 0-56.5 23.5T600-320q0 33 23.5 56.5T680-240ZM400-560q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Z"/></svg>
                   <span>Administration</span>
                </a>
             </li>
-            <li>
+            <li data-page="paiements">
                <a href="/pages/paiements">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M444-200h70v-50q50-9 86-39t36-89q0-42-24-77t-96-61q-60-20-83-35t-23-41q0-26 18.5-41t53.5-15q32 0 50 15.5t26 38.5l64-26q-11-35-40.5-61T516-710v-50h-70v50q-50 11-78 44t-28 74q0 47 27.5 76t86.5 50q63 23 87.5 41t24.5 47q0 33-23.5 48.5T486-314q-33 0-58.5-20.5T390-396l-66 26q14 48 43.5 77.5T444-252v52Zm36 120q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
                   <span>Paiements</span>
                </a>
             </li>
-            <li>
+            <li data-page="utilisateurs">
                <a href="/pages/utilisateurs">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg>
                   <span>Utilisateurs</span>
                </a>
             </li>
-            <li>
+            <li data-page="store">
                <a href="/pages/store">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M841-518v318q0 33-23.5 56.5T761-120H201q-33 0-56.5-23.5T121-200v-318q-23-21-35.5-54t-.5-72l42-136q8-26 28.5-43t47.5-17h556q27 0 47 16.5t29 43.5l42 136q12 39-.5 71T841-518Zm-272-42q27 0 41-18.5t11-41.5l-22-140h-78v148q0 21 14 36.5t34 15.5Zm-180 0q23 0 37.5-15.5T441-612v-148h-78l-22 140q-4 24 10.5 42t37.5 18Zm-178 0q18 0 31.5-13t16.5-33l22-154h-78l-40 134q-6 20 6.5 43t41.5 23Zm540 0q29 0 42-23t6-43l-42-134h-76l22 154q3 20 16.5 33t31.5 13ZM201-200h560v-282q-5 2-6.5 2H751q-27 0-47.5-9T663-518q-18 18-41 28t-49 10q-27 0-50.5-10T481-518q-17 18-39.5 28T393-480q-29 0-52.5-10T299-518q-21 21-41.5 29.5T211-480h-4.5q-2.5 0-5.5-2v282Zm560 0H201h560Z"/></svg>
                   <span>Magasin</span>
                </a>
             </li>
-            <li>
+            <li data-page="evenements">
                <a href="/pages/evenements">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z"/></svg>
                   <span>Evènements</span>
                </a>
             </li>
-            <li>
+            <li data-page="chat">
                <a href="/pages/chat">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>
                   <span>Messages</span>
@@ -120,7 +120,37 @@ import 'unofficial-pf-v5-wc-icons';
          border-radius: 10px;
 
          svg{
-            fill: #e6e6ef;
+            fill: #ffffff;
+         }
+      }
+      #sidebar ul li.active.light{
+         background-color: #5e63ff;
+         border-radius: 10px;
+         color: #ffffff;
+
+         svg{
+            fill: #ffffff
+         }
+      }
+      #sidebar ul li.active.light{
+         background-color: #5e63ff;
+         border-radius: 10px;
+         color: #ffffff;
+
+         svg{
+            fill: #ffffff
+         }
+      }
+      #sidebar ul li.active.light a{
+         color: #ffffff;
+      }
+      li.active {
+         background-color: #5e63ff;
+         border-radius: 10px;
+         color: #ffffff;
+
+         svg{
+            fill: #ffffff
          }
       }
       #sidebar a{
@@ -259,9 +289,12 @@ export class VerticalNavBar extends WebComponent {
    connectedCallback() {
       super.connectedCallback();
 
+      this.addClickListeners();
+      this.restoreActiveState();
+
       console.log('vertical navbar connecté');
 
-      this.onMounting()
+      this.onMounting();
       
 
       // Écoute l'événement 'close-navbars' sur le document
@@ -294,7 +327,51 @@ export class VerticalNavBar extends WebComponent {
       } else {
           console.log("Aucun état de mode sombre trouvé, utilisation de l'état par défaut.");
       }
-  }
+   }
+
+   // Méthode pour ajouter les écouteurs d'événements de clic
+   addClickListeners() {
+      const listItems = this.shadowRoot.querySelectorAll('li');
+      listItems.forEach((li) => {
+        li.addEventListener('click', this.handleItemClick.bind(this));
+      });
+   }
+
+ // Méthode pour gérer le clic sur un élément li
+ handleItemClick(event: Event) {
+
+   const navigation = JSON.parse(localStorage.getItem('navigation')) || {};
+   
+ 
+   const target = event.currentTarget as HTMLElement;
+ 
+   const listItems = this.shadowRoot.querySelectorAll('li');
+   listItems.forEach((li) => {
+     li.classList.remove('active');
+   });
+ 
+   target.classList.add('active');
+ 
+   const pageId = target.getAttribute('data-page');
+ 
+   if (pageId) {
+     navigation.activePage = pageId;
+     localStorage.setItem('navigation', JSON.stringify(navigation));
+   }
+ }
+ 
+
+   // Méthode pour restaurer l'état actif depuis le localStorage
+   restoreActiveState() {
+      const navigation = JSON.parse(localStorage.getItem('navigation')) || {};
+
+      if(navigation.activePage) {
+         const activeItem = this.shadowRoot.querySelector(`li[data-page="${navigation.activePage}"]`);
+         if (activeItem) {
+            activeItem.classList.add('active');
+         }
+      }
+   }
   
    handleMode(event: CustomEvent) {
       const navigation = JSON.parse(localStorage.getItem('navigation')) || {};
