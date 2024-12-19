611a73a0adf295e9e92c0ccaeb4e77fc72d90704
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2024-12-19 13:35:54 +0100

    update

--- dist/pages/index.5ff79fd3.js
+++ dist/pages/index.5ff79fd3.js
@@ -1684,51 +1684,51 @@ Inscription = (0, _tsDecorate._)([
             ${inscription.currentStep === 1 ? (0, _core.html)`
                   <h2>Informations personnelles</h2>
                   <div class="row">
-                      <div class="input-field">
-                        <label for="name">Nom</label>
-                        <input type="text" id="name" name="name" />
-                      </div>
-                      <div class="input-field">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" />
-                      </div>
+                    <div class="input-field">
+                      <label for="name">Nom</label>
+                      <input type="text" id="name" name="name" />
+                    </div>
+                    <div class="input-field">
+                      <label for="email">Email</label>
+                      <input type="email" id="email" name="email" />
+                    </div>
                   </div>
                   <div class="row">
-                      <div class="input-field">
-                        <label for="name">Prénom</label>
-                        <input type="text" id="name" name="name" />
-                      </div>
-                      <div class="input-field">
-                        <label for="email">Date de naissance</label>
-                        <input type="date" id="email" name="email" />
-                      </div>
+                    <div class="input-field">
+                      <label for="name">Prénom</label>
+                      <input type="text" id="name" name="name" />
+                    </div>
+                    <div class="input-field">
+                      <label for="email">Date de naissance</label>
+                      <input type="date" id="email" name="email" />
+                    </div>
                   </div>
                   <div class="row">
-                      <div class="input-field">
-                        <label for="genre">genre</label>
-                        <select id="genre" name="cars">
-                            <option value=""></option>
-                            <option value="homme">Homme</option>
-                            <option value="femme">Femme</option>
-                        </select>
-                      </div>
-                      <div class="input-field">
-                        <label for="tarifs">plan tarifaire</label>
-                        <select id="tarifs" name="cars">
-                            <option value=""></option>
-                            <option value="mensuel">mensuel : 30 euros</option>
-                            <option value="trimestriel">trimestriel : 100 euros</option>
-                            <option value="annuel">annuel : 250 euros</option>
-                        </select>
-                      </div>
+                    <div class="dropdown">
+                      <label for="genre">Genre</label>
+                      <select id="genre" name="cars">
+                        <option value=""></option>
+                        <option value="homme">Homme</option>
+                        <option value="femme">Femme</option>
+                      </select>
+                    </div>
+                    <div class="dropdown">
+                      <label for="tarifs">Plan tarifaire</label>
+                      <select id="tarifs" name="cars">
+                        <option value=""></option>
+                        <option value="mensuel">Mensuel : 30 euros</option>
+                        <option value="trimestriel">Trimestriel : 100 euros</option>
+                        <option value="annuel">Annuel : 250 euros</option>
+                      </select>
+                    </div>
                   </div>
                 ` : ""}
             ${inscription.currentStep === 2 ? (0, _core.html)`
                   <h2>Détails de connexion</h2>
                   <div class="row">
-                      <div class="input-field">
-                        <label for="username">Nom d'utilisateur</label>
-                        <input type="text" id="username" name="username" />
+                    <div class="input-field">
+                      <label for="username">Nom d'utilisateur</label>
+                      <input type="text" id="username" name="username" />
                     </div>
                     <div class="input-field">
                       <label for="password">Mot de passe</label>
@@ -1748,23 +1748,31 @@ Inscription = (0, _tsDecorate._)([
           </div>
 
           <div class="navigation">
-  <button
-    @click="${()=>inscription.goToStep(inscription.currentStep - 1)}"
-    ?disabled="${inscription.currentStep === 1}"
-  >
-    Précédent
-  </button>
-  ${inscription.currentStep === 3 ? (0, _core.html)`
-        <button @click="${inscription.submitForm}">Soumettre</button>
-      ` : (0, _core.html)`
-        <button
-          @click="${()=>inscription.goToStep(inscription.currentStep + 1)}"
-          ?disabled="${inscription.currentStep === 3}"
-        >
-          Suivant
-        </button>
-      `}
-</div>
+            <button
+              @click="${()=>inscription.goToStep(inscription.currentStep - 1)}"
+              ?disabled="${inscription.currentStep === 1}"
+            >
+              Précédent
+            </button>
+            ${inscription.currentStep === 3 ? (0, _core.html)`<button @click="${inscription.submitForm}">Soumettre</button>` : (0, _core.html)`
+                  <button
+                    @click="${()=>inscription.goToStep(inscription.currentStep + 1)}"
+                    ?disabled="${inscription.currentStep === 3}"
+                  >
+                    Suivant
+                  </button>
+                `}
+          </div>
+          <span id="divider">
+            <div class="line"></div>
+            <div class="divider-text">ou</div>
+            <div class="line"></div>
+          </span>
+          <div class="google-register">
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/></svg>
+              </button>
+          </div>
         </div>
       </div>
     `;
@@ -1772,7 +1780,7 @@ Inscription = (0, _tsDecorate._)([
         styles: [
             (0, _core.css)`
       #inscription {
-        height: 500px;
+        height: auto;
         justify-content: space-around;
         position: absolute;
         right: 25%;
@@ -1787,6 +1795,7 @@ Inscription = (0, _tsDecorate._)([
         flex-direction: column;
         background-color: #ffffff;
         gap: 20px;
+        height: 100%;
       }
       .step {
         display: flex;
@@ -1794,12 +1803,14 @@ Inscription = (0, _tsDecorate._)([
         margin-left: 15px;
         margin-right: 15px;
         gap: 20px;
-        height: 80%;
+        height: 400px;
+        width: 100%;
       }
-      .row{
+      .row {
         display: flex;
         align-items: center;
-        gap: 20px;
+        gap: 10px;
+        width: 100%;
       }
       .navigation {
         display: flex;
@@ -1818,130 +1829,143 @@ Inscription = (0, _tsDecorate._)([
       }
 
       /* Styles pour les indicateurs d'étapes et lignes connectées */
-    .step-indicators {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      position: relative;
-      margin-left: 15px;
-      margin-top: 15px;
-      margin-bottom: 15px;
-    }
-
-    .step-container {
-      display: flex;
-      align-items: center;
-    }
-
-    .step-indicator {
-      width: 40px;
-      height: 40px;
-      border-radius: 50%;
-      background-color: #ddd;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-weight: bold;
-      margin-right: 10px;
-    }
-
-    .step-indicator.active {
-      background-color: #4caf50;
-      color: white;
-    }
-
-    .step-name {
-      font-size: 16px;
-    }
-
-    .step-line {
-      width: 4px;
-      height: 110px; /* Ajustez la hauteur des lignes */
-      background-color: #ddd;
-      margin-left: 18px; /* Aligner avec les cercles */
-    }
-
-    .step-line.active {
-      background-color: #4caf50;
-    }
-    .input-field {
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-    }
-    .input-field label{
-      color: #b1b3bc;
-      font-size: 13px;
-    }
-    input[type="text"]{
-      padding: 10px 10px;
-      width: 27ch;
-      border: 1px solid #e2e8f1;
-      border-radius: 5px;
-    }
-    input[type="email"]{
-      padding: 10px 10px;
-      width: 27ch;
-      border: 1px solid #e2e8f1;
-      border-radius: 5px;
-    }
-    input[type="password"]{
-      padding: 10px 10px;
-      width: 27ch;
-      border: 1px solid #e2e8f1;
-      border-radius: 5px;
-    }
-    input[type="date"]{
-      padding: 9px 10px;
-      width: 27ch;
-      border: 1px solid #e2e8f1;
-      border-radius: 5px;
-      color: #b1b3bc;
-
-      svg{
-        fill: #b1b3bc;
+      .step-indicators {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        position: relative;
+        margin-left: 15px;
+        margin-top: 15px;
+        margin-bottom: 15px;
       }
-    }
-  
-    input[type="date"]::placeholder {
-      color: #888;
-    }
 
-    input[type="date"]:focus, input[type="password"]:focus, input[type="text"]:focus, input[type="email"]:focus, select {
-      border-color: #005eff;
-      outline: none;
-    }
-    
-    select {
-      padding: 9px 10px;
-      width: 25ch;
-      border-radius: 5px;
-      background-color: #ffffff;
-      border: 1px solid #e2e8f1;
-      font-size: 16px;
-      color: #333;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      cursor: pointer;
-      position: relative;
-    }
+      .step-container {
+        display: flex;
+        align-items: center;
+      }
 
-/* Ajouter une icône de flèche personnalisée */
-select::after {
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    pointer-events: none; /* Ignore les clics pour permettre d'interagir avec le select */
-}
+      .step-indicator {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background-color: #ddd;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: bold;
+        margin-right: 10px;
+      }
 
-/* Styliser les options (limité dans CSS natif) */
-option {
-    background-color: white;
-    color: black;
-    padding: 10px;
-}
+      .step-indicator.active {
+        background-color: #4caf50;
+        color: white;
+      }
 
+      .step-name {
+        font-size: 16px;
+      }
+
+      .step-line {
+        width: 4px;
+        height: 110px; /* Ajustez la hauteur des lignes */
+        background-color: #ddd;
+        margin-left: 18px; /* Aligner avec les cercles */
+      }
+
+      .step-line.active {
+        background-color: #4caf50;
+      }
+
+      .input-field {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+      }
+      .input-field label {
+        color: #b1b3bc;
+        font-size: 13px;
+      }
+      input[type='text'] {
+        padding: 10px 10px;
+        width: 27ch;
+        border: 1px solid #e2e8f1;
+        border-radius: 5px;
+      }
+      input[type='email'] {
+        padding: 10px 10px;
+        width: 27ch;
+        border: 1px solid #e2e8f1;
+        border-radius: 5px;
+      }
+      input[type='password'] {
+        padding: 10px 10px;
+        width: 27ch;
+        border: 1px solid #e2e8f1;
+        border-radius: 5px;
+      }
+      input[type='date'] {
+        padding: 9px 10px;
+        width: 27ch;
+        border: 1px solid #e2e8f1;
+        border-radius: 5px;
+        color: #b1b3bc;
+
+        svg {
+          fill: #b1b3bc;
+        }
+      }
+
+      input[type='date']::placeholder {
+        color: #888;
+      }
+
+      input[type='date']:focus,
+      input[type='password']:focus,
+      input[type='text']:focus,
+      input[type='email']:focus,
+      select {
+        border-color: #005eff;
+        outline: none;
+      }
+      .dropdown{
+        color: #b1b3bc;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+      }
+      select {
+        padding: 9px 10px;
+        width: 25ch;
+        border-radius: 5px;
+        background-color: #ffffff;
+        border: 1px solid #e2e8f1;
+        color: #b1b3bc;
+      }
+      span#divider {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        gap: 10px;
+        margin-left: 15px;
+        margin-right: 15px;
+      }
+      span#divider > .line {
+        width: 100%;
+        height: 1px;
+        background-color: #e2e8f1;
+      }
+      .google-register {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 15px;
+      }
+      .google-register > button{
+        background-color: #ffffff;
+        box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+        width: 70px;
+      }
     `
         ]
     })
